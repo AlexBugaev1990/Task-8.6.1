9fedc0b85bdd47232117f14bf34970fcabc871b0
AlexBugaev1990 <alex.bugaev90@gmail.com>
2022-05-25 22:35:56 +0300

    Добавьте файлы проекта.

---
+++ Task 8.6.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Task_8._6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к файлам");
+            string Path = Console.ReadLine();
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(Path);
+                if (dirInfo.Exists)
+                {
+                    foreach (FileInfo file in dirInfo.GetFiles())
+                        file.Delete();
+                    foreach (DirectoryInfo dir in dirInfo.GetDirectories())
+                    {
+                        dir.Delete(true);
+                    }
+                    Console.WriteLine("Файлы и папки успешно удалены");
+                }
+                else
+                {
+                    Console.WriteLine("Путь указан не верно");
+                }
+                
+            }
+            
+             catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
